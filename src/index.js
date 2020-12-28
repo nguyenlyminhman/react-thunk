399dfa96e8048fd0e44efffdfec94cf42f86f9db
Man Nguyen <nguyenlyminhman@gmail.com>
2020-12-29 05:29:37 +0700

    Add provider store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider as StoreProvider } from 'react-redux';
+import { store } from '../src/store';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StoreProvider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StoreProvider>,
   document.getElementById('root')
 );
 
